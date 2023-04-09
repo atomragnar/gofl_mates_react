48c0731971fe684e7550fb4ba451ed70ef8be6c5
atomragnar <johanneshornbrinck@gmail.com>
2023-04-09 22:27:18 +0200

    inkl landing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,6 @@ export const HomePage = () => {
             <News />
             <Pricing />
             <Contact />
-            <Footer />
-
         </>
     )
 }
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -31,6 +31,7 @@ function RootLayout() {
             <main>
             <Outlet/>
             </main>
+            <Footer/>
         </>
     );
 }
